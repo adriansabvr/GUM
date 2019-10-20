cc64f033ad4e5ca6332acc0ba6897aaba32c9b8a
Adrian <vitalreminds@gmail.com>
2019-10-19 20:46:51 -0500

    Heroku conf

--- shopping_cart/settings.py
+++ shopping_cart/settings.py
@@ -160,7 +160,6 @@ STATIC_URL = '/static/'
 # Extra lookup directories for collectstatic to find static files
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
